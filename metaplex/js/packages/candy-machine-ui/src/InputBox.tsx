42a8f3aae1bd0b1896443396a7ec13239e5d4372
sert121 <yashrajmore29@gmail.com>
2022-04-09 17:48:32 +0530

    archiiiiving

---
+++ metaplex/js/packages/candy-machine-ui/src/InputBox.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import "./Inputbox.css";
+
+import logo from "./images/logo.png";
+import logo3 from "./images/logo3.png";
+
+import './header.css';
+
+const InputBox = () => {
+    const [input, setInput] = React.useState("");
+     const [placeholder,setPlace] = React.useState("Enter candy machine ID");
+
+    const handleSubmit = (e:any) => {
+		e.preventDefault();
+        let s = window.location.origin +  "/"+ input;
+		window.location.href = s;
+        setInput("");
+        
+        
+    }
+    
+	const handlekeyPress = (e:any) => {
+        if (e.key === 'Enter') {
+		e.preventDefault();
+        let s = window.location.origin  + "/"+ input;
+		window.location.href = s;
+		// window.location.href = s;
+        setInput("");}
+    }   
+ 
+  return (
+      <div  className="outermist">
+            <div className="header">
+                    <a href="#default" className="logo_url">
+                        <img src={logo} className="logo">
+
+                        </img>
+                    </a>
+                    <div className="header-right">
+                        <a className="active" href="#home">Home</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
+                    </div>
+            </div>
+        
+      <div className='container-input'>
+            <div className='input-parent' >
+                <div>
+                    <img src={logo3} className="logo2">
+                    </img>
+                </div>
+                
+                <div>
+                <input type="text"
+                 placeholder={placeholder}
+                 onFocus={(e) => setPlace('')}
+                 onBlur={(e) => setPlace('Enter candy machine ID')}
+                 onChange={(e) => setInput(e.target.value)}
+                 onKeyPress={handlekeyPress}
+                 onSubmit={handleSubmit}
+                 className='input-box' 
+                ></input>
+                </div>
+            </div>
+
+     </div>
+     </div>
+  )
+};
+
+export default InputBox;
